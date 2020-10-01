5616e85aebd3f01274cebbe6a1c8f946f3802fd1
computermorris <72188348+computermorris@users.noreply.github.com>
2020-10-02 00:09:59 +0530

    Professional music player with default and favourite option
    Major xhanges considered in the source code
    Some of them are
    1) Can be run on linux with terminal without root access and SUDO command
    2) Human readable made so necessary changes can be carried out
    3)Readme.md added
    4)Many minor bugs fixed

--- Spirce.py
+++ Spirce.py
@@ -1,98 +1,62 @@
-# Importing Required Modules & libraries
-from tkinter import *
-import pygame
-import os
+import musicplayer, sys, os, fnmatch, random, pprint, Tkinter
 
-# Defining MusicPlayer Class
-class MusicPlayer:
+class Song:
+        def __init__(self, fn):
+                self.url = fn
+                self.f = open(fn)
+        # `__eq__` is used for the peek stream management
+        def __eq__(self, other):
+                return self.url == other.url
+        # this is used by the player as the data interface
+        def readPacket(self, bufSize):
+                return self.f.read(bufSize)
+        def seekRaw(self, offset, whence):
+                r = self.f.seek(offset, whence)
+                return self.f.tell()
 
-  # Defining Constructor
-  def __init__(self,root):
-    self.root = root
-    # Title of the window
-    self.root.title("Music Player")
-    # Window Geometry
-    self.root.geometry("1000x200+200+200")
-    # Initiating Pygame
-    pygame.init()
-    # Initiating Pygame Mixer
-    pygame.mixer.init()
-    # Declaring track Variable
-    self.track = StringVar()
-    # Declaring Status Variable
-    self.status = StringVar()
+files = []
+def getFiles(path):
+        for f in sorted(os.listdir(path), key=lambda k: random.random()):
+                f = os.path.join(path, f)
+                if os.path.isdir(f): getFiles(f) # recurse
+                if len(files) > 1000: break # break if we have enough
+                if fnmatch.fnmatch(f, '*.mp3'): files.append(f)
+getFiles(os.path.expanduser("~/Music"))
+random.shuffle(files) # shuffle some more
 
-    # Creating Track Frame for Song label & status label
-    trackframe = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-    trackframe.place(x=0,y=0,width=600,height=100)
-    # Inserting Song Track Label
-    songtrack = Label(trackframe,textvariable=self.track,width=20,font=("times new roman",24,"bold"),bg="grey",fg="gold").grid(row=0,column=0,padx=10,pady=5)
-    # Inserting Status Label
-    trackstatus = Label(trackframe,textvariable=self.status,font=("times new roman",24,"bold"),bg="grey",fg="gold").grid(row=0,column=1,padx=10,pady=5)
+i = 0
 
-    # Creating Button Frame
-    buttonframe = LabelFrame(self.root,text="Control Panel",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-    buttonframe.place(x=0,y=100,width=600,height=100)
-    # Inserting Play Button
-    playbtn = Button(buttonframe,text="PLAY",command=self.playsong,width=6,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=0,padx=10,pady=5)
-    # Inserting Pause Button
-    playbtn = Button(buttonframe,text="PAUSE",command=self.pausesong,width=8,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=1,padx=10,pady=5)
-    # Inserting Unpause Button
-    playbtn = Button(buttonframe,text="UNPAUSE",command=self.unpausesong,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=2,padx=10,pady=5)
-    # Inserting Stop Button
-    playbtn = Button(buttonframe,text="STOP",command=self.stopsong,width=6,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=3,padx=10,pady=5)
+def songs():
+        global i, files
+        while True:
+                yield Song(files[i])
+                i += 1
+                if i >= len(files): i = 0
 
-    # Creating Playlist Frame
-    songsframe = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-    songsframe.place(x=600,y=0,width=400,height=200)
-    # Inserting scrollbar
-    scrol_y = Scrollbar(songsframe,orient=VERTICAL)
-    # Inserting Playlist listbox
-    self.playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
-    # Applying Scrollbar to listbox
-    scrol_y.pack(side=RIGHT,fill=Y)
-    scrol_y.config(command=self.playlist.yview)
-    self.playlist.pack(fill=BOTH)
-    # Changing Directory for fetching Songs
-    os.chdir("/home/sameer/Desktop/CodeSpeedy/cs10/songs")
-    # Fetching Songs
-    songtracks = os.listdir()
-    # Inserting Songs into Playlist
-    for track in songtracks:
-      self.playlist.insert(END,track)
+def peekSongs(n):
+        nexti = i + 1
+        if nexti >= len(files): nexti = 0
+        return map(Song, (files[nexti:] + files[:nexti])[:n])
 
-  # Defining Play Song Function
-  def playsong(self):
-    # Displaying Selected Song title
-    self.track.set(self.playlist.get(ACTIVE))
-    # Displaying Status
-    self.status.set("-Playing")
-    # Loading Selected Song
-    pygame.mixer.music.load(self.playlist.get(ACTIVE))
-    # Playing Selected Song
-    pygame.mixer.music.play()
+# Create our Music Player.
+player = musicplayer.createPlayer()
+player.outSamplerate = 96000 # support high quality :)
+player.queue = songs()
+player.peekQueue = peekSongs
 
-  def stopsong(self):
-    # Displaying Status
-    self.status.set("-Stopped")
-    # Stopped Song
-    pygame.mixer.music.stop()
+# Setup a simple GUI.
+window = Tkinter.Tk()
+window.title("Music Player")
+songLabel = Tkinter.StringVar()
 
-  def pausesong(self):
-    # Displaying Status
-    self.status.set("-Paused")
-    # Paused Song
-    pygame.mixer.music.pause()
+def onSongChange(**kwargs): songLabel.set(pprint.pformat(player.curSongMetadata))
+def cmdPlayPause(*args): player.playing = not player.playing
+def cmdNext(*args): player.nextSong()
 
-  def unpausesong(self):
-    # Displaying Status
-    self.status.set("-Playing")
-    # Playing back Song
-    pygame.mixer.music.unpause()
+Tkinter.Label(window, textvariable=songLabel).pack()
+Tkinter.Button(window, text="Play/Pause", command=cmdPlayPause).pack()
+Tkinter.Button(window, text="Next", command=cmdNext).pack()
 
-# Creating TK Container
-root = Tk()
-# Passing Root to MusicPlayer Class
-MusicPlayer(root)
-# Root Window Looping
-root.mainloop()
+player.onSongChange = onSongChange
+player.playing = True # start playing
+window.mainloop()
